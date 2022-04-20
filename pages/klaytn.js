69b2dc15c4baf9ef1d1cf5d947ce0f0b5046f828
sb.park <been778@gmail.com>
2022-04-21 08:58:42 +0900

    로고 추가

--- pages/klaytn.js
+++ pages/klaytn.js
@@ -6,8 +6,8 @@ const Klaytn = () => {
 
     useEffect(async () => {
 
-        const tokenAddress = '0xcac25b35f3708acac59c505228597b0b664bc640'
-        const tokenSymbol = 'SBPT'
+        const tokenAddress = '0xce76ccddcabd6bc89da0acb0fa73b75a7ce509bc'
+        const tokenSymbol = 'SBT'
         const tokenDecimals = 8
         const tokenImage = 'https://www.playgrounds.co.kr/images/logo.png'
         if (typeof window.klaytn !== 'undefined') {
@@ -20,28 +20,28 @@ const Klaytn = () => {
                 // Currently only one:
                 // ['0xFDEa65C8e26263F6d9A1B5de9555D2931A33b825']
 
-                // klaytn.sendAsync(
-                //     {
-                //         method: 'wallet_watchAsset',
-                //         params: {
-                //             type: 'ERC20', // Initially only supports ERC20, but eventually more!
-                //             options: {
-                //                 address: tokenAddress, // The address that the token is at.
-                //                 symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-                //                 decimals: tokenDecimals, // The number of decimals in the token
-                //                 image: tokenImage // A string url of the token logo
-                //             }
-                //         },
-                //         id: Math.round(Math.random() * 100000)
-                //     },
-                //     (err, added) => {
-                //         if (added) {
-                //             console.log('Thanks for your interest!')
-                //         } else {
-                //             console.log('Your loss!')
-                //         }
-                //     }
-                // )
+                klaytn.sendAsync(
+                    {
+                        method: 'wallet_watchAsset',
+                        params: {
+                            type: 'ERC20', // Initially only supports ERC20, but eventually more!
+                            options: {
+                                address: tokenAddress, // The address that the token is at.
+                                symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
+                                decimals: tokenDecimals, // The number of decimals in the token
+                                image: tokenImage // A string url of the token logo
+                            }
+                        },
+                        id: Math.round(Math.random() * 100000)
+                    },
+                    (err, added) => {
+                        if (added) {
+                            console.log('Thanks for your interest!')
+                        } else {
+                            console.log('Your loss!')
+                        }
+                    }
+                )
             } catch (error) {
                 // Handle error. Likely the user rejected the login
                 console.error(error)
